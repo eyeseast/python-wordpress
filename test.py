8046b078a94a1920054519e6c639e9db8b6e22d2
Chris Amico <eyeseast@gmail.com>
2011-03-13 17:22:34 -0400

    Looks like this pretty much works

---
+++ test.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import httplib2
+import os
+import unittest
+import urlparse
+from wordpress import WordPress, json, DEFAULT_METHODS
+
+http = httplib2.Http()
+
+class WordPressTest(unittest.TestCase):
+    
+    def setUp(self):
+        self.blog_url = "http://localhost:8888/wordpress"
+        self.wp = WordPress(self.blog_url, cache=None)
+    
+    def check_response(self, result, url):
+        "Check that the parsed result matches the given URL"
+        r, c = http.request(url)
+        c = json.loads(c)
+        self.assertEqual(c, result)
+        
+    def test_get_info(self):
+        "Test basic info"
+        result = self.wp.info()
+        self.check_response(result, 'http://localhost:8888/wordpress/?dev=1&json=info')
+    
+    def test_get_recent_posts(self):
+        "Test getting recent posts"
+        result = self.wp.get_recent_posts()
+        self.check_response(result, 'http://localhost:8888/wordpress/api/get_recent_posts/?dev=1')
+    
+    def test_get_post(self):
+        """Test getting a single post"""
+        result = self.wp.get_post(id=1)
+        self.check_response(result, 'http://localhost:8888/wordpress/?json=get_post&id=1&dev=1')
+        
+    def test_get_categories(self):
+        """Test getting all active categories"""
+        result = self.wp.get_category_index()
+        self.check_response(result, 'http://localhost:8888/wordpress/?json=get_category_index&dev=1')
+    
+
+if __name__ == '__main__':
+    unittest.main()
--- wordpress.py
+++ wordpress.py
@@ -5,26 +5,90 @@ A Python client for the WordPress JSON API
 # Python-based projects and a wordpress installation.
 
 import httplib2
+import os
 import urllib
+import urlparse
 try:
     import json
 except ImportError:
     import simplejson as json
 
+__all__ = ('WordPress', 'WordPressError')
+
 # Setting debug to True will add requested URLs to returned JSON
 DEBUG = False
 
+DEFAULT_METHODS = [
+    'info',
+    'get_recent_posts',
+    'get_post',
+    'get_page',
+    'get_date_posts',
+    'get_category_posts',
+    'get_tag_posts',
+    'get_author_posts',
+    'get_search_results',
+    'get_date_index',
+    'get_category_index',
+    'get_tag_index',
+    'get_author_index',
+    'get_page_index',
+    # 'get_nonce'
+]
+
+class WordPressError(Exception):
+    """
+    An error for things that break with WordPress
+    """
+
 # ## WordPress
-# Create an instance with a blog's URL, path prefix (`api` by default)
-# and cache setting. WordPress uses httplib2 under the hood, and caching
+# Create an instance with a blog's URL, and cache setting. WordPress uses 
+# [httplib2](http://code.google.com/p/httplib2/) under the hood, and caching
 # is pluggable. It's possible to pass in a `django.core.cache.cache` object
 # here to use the same cache backend you've set up for a Django project.
+# 
+# See [httplib2 docs](http://httplib2.googlecode.com/hg/doc/html/libhttplib2.html#id1)
+# for more on cache backends.
 class WordPress(object):
     """
     The main wrapper
     """
-    def __init__(self, blog_url, prefix='api', cache='.cache'):
+    def __init__(self, blog_url, cache='.cache'):
+        self.dev = 1
         self.blog_url = blog_url
-        self.prefix = prefix
-        self.base_url = urlparse.urljoin(blog_url, prefix)
+        if not self.blog_url.endswith('?'):
+            self.blog_url += "?"
         self._http = httplib2.Http(cache)
+    
+    # fetch does most of the dirty work. Pass in an API method and
+    # some kwargs and it grabs, parses and returns some useful JSON
+    def fetch(self, method, **kwargs):
+        "Grab, parse and return the actual response"
+        kwargs.setdefault('dev', self.dev)
+        kwargs['json'] = method
+        url = self.blog_url + urllib.urlencode(kwargs)
+        
+        r, c = self._http.request(url)
+        c = json.loads(c)
+        if c.get('status', '').lower() == "error" or "error" in c:
+            raise WordPressError(c.get('error'))
+        
+        if DEBUG:
+            c['_url'] = url
+        return c
+        
+    # The WordPress JSON API is really simple. Give it a named method
+    # in a query string--`?json=get_recent_posts`--and it returns JSON. 
+    # Since all we're doing is passing methods and arguments, there's
+    # little point re-defining those methods in Python
+    def __getattr__(self, method):
+        """
+        Return a callable API method if `method` is in self._methods
+        """
+        if method in DEFAULT_METHODS:
+            def _api_method(**kwargs):
+                return self.fetch(method, **kwargs)
+            _api_method.__name__ = method
+            return _api_method
+        else:
+            raise AttributeError
